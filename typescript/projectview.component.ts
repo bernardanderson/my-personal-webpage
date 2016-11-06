3a4d4e7a93576d4c6b5df29b0094aecb1fb147ed
Bernard Anderson <bernard.anderson@gmail.com>
2016-11-05 23:51:47 -0500

    Added styling to first capstone item

--- typescript/projectview.component.ts
+++ typescript/projectview.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+
+declare var $:any;
 
 @Component({
     selector: 'project-view', 
@@ -8,8 +11,38 @@ import { Component } from '@angular/core';
 
 export class ProjectViewComponent {
   
-    flipper() {
-        console.log("You Flipped!");
-    }
+  // currentImageElement = 0;
+  
+  // checkyourselfImages = [
+  //   "AccountLedger.png",
+  //   "SingleItem.png",
+  //   "CheckBuilder.png",
+  //   "AccountExpend.png"
+  // ]
 
+  ngOnInit(){
+    $('.shape').shape();
+    // let pictureTimer = Observable.timer(3000, 6000);
+    // pictureTimer.subscribe(t => this.pictureChange());
+  }
+
+  flipper() {
+    $('.shape').shape('flip left');
+  }
+
+  // pictureChange() {
+  //   if (this.currentImageElement === 3) {
+  //     this.currentImageElement = 0;
+  //   } else {
+  //     this.currentImageElement++;
+  //   }
+  // }
+
+  hover():void {
+      $('.dimmer').dimmer('toggle');
+  }
+
+  blur():void {
+      $('.dimmer').dimmer('toggle');
+  }
 }
